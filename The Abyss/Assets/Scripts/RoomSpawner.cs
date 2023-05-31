dd1c0e3fa04d7b1c692cf804b41c329127544342
Kaartik Servansingh <kaartiks@matcon.ca>
2023-05-31 14:04:56 -0700

    Commit 4
    Added Room Generation Script and Room Template Scripts. Generation successful but buggy

--- The Abyss/Assets/Scripts/RoomSpawner.cs
+++ The Abyss/Assets/Scripts/RoomSpawner.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int openingDirection;
+    private RoomTemplates roomTemplates;
+    private int randInt;
+    public bool spawned = false;
+
+    private void Start()
     {
-        
+        roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        if (spawned == false) 
+        {
+            if (openingDirection == 1)
+            {
+                randInt = Random.Range(0, roomTemplates.bottomRooms.Length);
+                Instantiate(roomTemplates.bottomRooms[randInt], transform.position, roomTemplates.bottomRooms[randInt].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                randInt = Random.Range(0, roomTemplates.topRooms.Length);
+                Instantiate(roomTemplates.topRooms[randInt], transform.position, roomTemplates.topRooms[randInt].transform.rotation);
+
+            }
+            else if (openingDirection == 3)
+            {
+                randInt = Random.Range(0, roomTemplates.leftRooms.Length);
+                Instantiate(roomTemplates.leftRooms[randInt], transform.position, roomTemplates.leftRooms[randInt].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                randInt = Random.Range(0, roomTemplates.rightRooms.Length);
+                Instantiate(roomTemplates.rightRooms[randInt], transform.position, roomTemplates.rightRooms[randInt].transform.rotation);
+            }
+        }
+        spawned = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned != true) 
+        {
+            Destroy(gameObject);
+        }
     }
 }
