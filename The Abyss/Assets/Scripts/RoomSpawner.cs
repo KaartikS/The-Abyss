7a2f0d62a8ebba8ec670659205b122b8ace49735
Kaartik Servansingh <kaartiks@matcon.ca>
2023-06-01 10:25:22 -0700

    Commit 5
    Fixed room generator

--- The Abyss/Assets/Scripts/RoomSpawner.cs
+++ The Abyss/Assets/Scripts/RoomSpawner.cs
@@ -47,7 +47,7 @@ public class RoomSpawner : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned != true) 
+        if(other.CompareTag("SpawnPoint")) 
         {
             Destroy(gameObject);
         }
